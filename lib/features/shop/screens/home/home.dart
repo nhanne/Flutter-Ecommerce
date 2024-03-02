a304a48b9f5d34490a9e0dc2104bb3db3c72c027
Trần Thanh Nhân <90546715+nhanne@users.noreply.github.com>
2024-03-02 19:51:01 +0700

    Clip Path

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,3 +1,6 @@
+import 'package:clothes/common/widgets/custom_shapes/containers/circular_container.dart';
+import 'package:clothes/common/widgets/custom_shapes/curved_edges/curved_edges_widget.dart';
+import 'package:clothes/util/constants/colors.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -5,6 +8,37 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: SingleChildScrollView(
+          child: Column(
+        children: [
+          TCurvedEdgeWidget(
+            child: Container(
+              color: TColors.primary,
+              padding: const EdgeInsets.all(0),
+              child: SizedBox(
+                height: 400,
+                child: Stack(
+                  children: [
+                    Positioned(
+                        top: -150,
+                        right: -250,
+                        child: TCircularContainer(
+                            backgroundColor:
+                                TColors.textWhite.withOpacity(0.1))),
+                    Positioned(
+                        top: 100,
+                        right: -300,
+                        child: TCircularContainer(
+                            backgroundColor:
+                                TColors.textWhite.withOpacity(0.1))),
+                  ],
+                ),
+              ),
+            ),
+          )
+        ],
+      )),
+    );
   }
 }
