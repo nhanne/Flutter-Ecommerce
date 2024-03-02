e5a7c36f4cc5eb03a724b3b1fce9b38b8a1abec1
Trần Thanh Nhân <90546715+nhanne@users.noreply.github.com>
2024-03-02 20:22:06 +0700

    App Bar

---
+++ lib/common/widgets/custom_shapes/containers/primary_header_container.dart
@@ -0,0 +1,39 @@
+import 'package:clothes/common/widgets/custom_shapes/curved_edges/curved_edges_widget.dart';
+import 'package:clothes/util/constants/colors.dart';
+import 'package:flutter/material.dart';
+import 'circular_container.dart';
+
+class TPrimaryHeaderContainer extends StatelessWidget {
+  const TPrimaryHeaderContainer({
+    super.key,
+    required this.child,
+  });
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return TCurvedEdgeWidget(
+      child: SizedBox(
+        height: 400,
+        child: Container(
+          color: TColors.primary,
+          child: Stack(
+            children: [
+              Positioned(
+                  top: -150,
+                  right: -250,
+                  child: TCircularContainer(
+                      backgroundColor: TColors.textWhite.withOpacity(0.1))),
+              Positioned(
+                  top: 100,
+                  right: -300,
+                  child: TCircularContainer(
+                      backgroundColor: TColors.textWhite.withOpacity(0.1))),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,44 +1,26 @@
-import 'package:clothes/common/widgets/custom_shapes/containers/circular_container.dart';
-import 'package:clothes/common/widgets/custom_shapes/curved_edges/curved_edges_widget.dart';
-import 'package:clothes/util/constants/colors.dart';
+import 'package:clothes/common/widgets/custom_shapes/containers/primary_header_container.dart';
 import 'package:flutter/material.dart';
+import 'home_appbar.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: SingleChildScrollView(
-          child: Column(
-        children: [
-          TCurvedEdgeWidget(
-            child: Container(
-              color: TColors.primary,
-              padding: const EdgeInsets.all(0),
-              child: SizedBox(
-                height: 400,
-                child: Stack(
-                  children: [
-                    Positioned(
-                        top: -150,
-                        right: -250,
-                        child: TCircularContainer(
-                            backgroundColor:
-                                TColors.textWhite.withOpacity(0.1))),
-                    Positioned(
-                        top: 100,
-                        right: -300,
-                        child: TCircularContainer(
-                            backgroundColor:
-                                TColors.textWhite.withOpacity(0.1))),
-                  ],
-                ),
+        child: Column(
+          children: [
+            TPrimaryHeaderContainer(
+              child: Column(
+                children: [
+                  THomeAppBar(),
+                ],
               ),
             ),
-          )
-        ],
-      )),
+          ],
+        ),
+      ),
     );
   }
 }
