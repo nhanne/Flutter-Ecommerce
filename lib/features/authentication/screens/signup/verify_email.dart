593d14c3294634ef0442ed67e3e91a928300eac4
Trần Thanh Nhân <90546715+nhanne@users.noreply.github.com>
2024-03-03 10:08:51 +0700

    Verify_email Screen

--- lib/features/authentication/screens/signup/verify_email.dart
+++ lib/features/authentication/screens/signup/verify_email.dart
@@ -1,1 +1,51 @@
+import 'package:clothes/common/widgets/success_screen/success_screen.dart';
+import 'package:clothes/features/authentication/screens/login/login.dart';
+import 'package:clothes/util/constants/image_strings.dart';
+import 'package:clothes/util/constants/sizes.dart';
+import 'package:clothes/util/constants/text_strings.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class VerifyEmailScreen extends StatelessWidget {
+  const VerifyEmailScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        actions: [
+          IconButton(
+              onPressed: () => Get.offAll(() => const LoginScreen()),
+              icon: const Icon(CupertinoIcons.clear))
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(TSizes.defaultSpace),
+          child: Column(
+            children: [
+              /// Button
+              SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
                       onPressed: () => Get.to(() => SuccessScreen(
+                        image: TImages.staticSuccessIllustration,
+                        title: TTexts.yourAccountCreatedTitle,
+                        subTitle: TTexts.yourAccountCreatedSubTitle,
+                        onPressed: () => Get.to(() => const LoginScreen()),
+                      )),
+                      child: const Text(TTexts.tContinue))),
+              const SizedBox(height: TSizes.spaceBtwItems),
+              SizedBox(
+                  width: double.infinity,
+                  child: TextButton(
+                      onPressed: () {}, child: const Text(TTexts.resendEmail))),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
