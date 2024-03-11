fc970c7f44dd4ec498f8bc476287c2a58fd2f2c2
Trần Thanh Nhân <90546715+nhanne@users.noreply.github.com>
2024-03-11 20:47:08 +0700

    Tab Bar Scrolling

---
+++ lib/common/widgets/brands/brand_show_case.dart
@@ -0,0 +1,47 @@
+import 'package:clothes/common/widgets/custom_shapes/containers/rounded_container.dart';
+import 'package:clothes/util/constants/colors.dart';
+import 'package:clothes/util/constants/image_strings.dart';
+import 'package:clothes/util/constants/sizes.dart';
+import 'package:clothes/util/helpers/helper_functions.dart';
+import 'package:flutter/material.dart';
+
+import 'brand_cart.dart';
+
+class TBrandShowcase extends StatelessWidget {
+  const TBrandShowcase({
+    super.key,
+    required this.images,
+  });
+  final List<String> images;
+
+  @override
+  Widget build(BuildContext context) {
+    return TRoundedContainer(
+      showBorder: true,
+      borderColor: TColors.darkGrey,
+      backgroundColor: Colors.transparent,
+      padding: const EdgeInsets.all(TSizes.md),
+      margin: const EdgeInsets.only(bottom: TSizes.spaceBtwItems),
+      child: Column(
+        children: [
+          const TBrandCard(showBorder: false),
+          Row(
+            children: images.map((image) => brandTopProductImageWidget(image, context)).toList(),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget brandTopProductImageWidget(String image, context){
+    return Expanded(
+      child: TRoundedContainer(
+        height: 100,
+        padding: const EdgeInsets.all(TSizes.md),
+        margin: const EdgeInsets.only(right: TSizes.sm),
+        backgroundColor: THelperFunctions.isDarkMode(context) ? TColors.darkerGrey : TColors.light,
+        child: Image(fit: BoxFit.contain, image: AssetImage(image)),
+      ),
+    );
+  }
+}
