f57ae76f8edc575d71bfb43452908c9ed57efe30
Trần Thanh Nhân <90546715+nhanne@users.noreply.github.com>
2024-03-17 10:30:16 +0700

    Profile Screen

--- lib/common/widgets/list_tiles/user_profile_tile.dart
+++ lib/common/widgets/list_tiles/user_profile_tile.dart
@@ -6,16 +6,16 @@ import 'package:iconsax/iconsax.dart';
 
 class TUserProfileTitle extends StatelessWidget {
   const TUserProfileTitle({
-    super.key,
+    super.key, required this.onPressed,
   });
-
+  final VoidCallback onPressed;
   @override
   Widget build(BuildContext context) {
     return ListTile(
       leading: const TCircularImage(image: TImages.user, width: 50, height: 50, padding: 0),
       title: Text('Trần Thanh Nhân', style: Theme.of(context).textTheme.headlineSmall!.apply(color: TColors.white)),
       subtitle: Text('nhantran.netdev@gmail.com',style: Theme.of(context).textTheme.bodyMedium!.apply(color: TColors.white)),
-      trailing: IconButton(onPressed: (){}, icon: const Icon(Iconsax.edit, color: TColors.white)),
+      trailing: IconButton(onPressed: onPressed, icon: const Icon(Iconsax.edit, color: TColors.white)),
     );
   }
 }
